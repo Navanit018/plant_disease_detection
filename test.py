def1c8f0162191cb915467bdaeb69d22069e1900
Navanit Sharma <navanit.231519@ncit.edu.np>
2026-01-14 21:22:25 +0545

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+
+# Specify the parent directory to check
+parent_dir = "C:/Users/ACER/OneDrive/Documents/plant_disease_project/"
+
+# List all files and directories in the specified path
+print("Contents of the directory:")
+print(os.listdir(parent_dir))
