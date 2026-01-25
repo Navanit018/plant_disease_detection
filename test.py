94d650302876be46b30fcd8b4af6e8d0bb63f472
Navanit Sharma <navanit.231519@ncit.edu.np>
2026-01-25 19:37:21 +0545

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import os
 
-# Specify the parent directory to check
+
 parent_dir = "C:/Users/ACER/OneDrive/Documents/plant_disease_project/"
 
 
 print("Contents of the directory:")
 
 print(os.listdir(parent_dir))
+
